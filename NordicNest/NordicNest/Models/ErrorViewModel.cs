b8aa9557ac58ca6284adc1ca622f988614eedb90
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-12-16 20:59:46 +0100

    ASP.NET Created

---
+++ NordicNest/NordicNest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NordicNest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
