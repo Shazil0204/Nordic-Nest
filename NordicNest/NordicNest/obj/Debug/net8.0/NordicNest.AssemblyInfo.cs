99467e333b3e218d397411e66a038f63daebbe3c
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-12-18 15:46:01 +0100

    Navbar

--- NordicNest/NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
+++ NordicNest/NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a1605f81ffe9dd553772ce53092508d3988674")]
 [assembly: System.Reflection.AssemblyProductAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
