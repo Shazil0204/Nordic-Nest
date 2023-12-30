ebff34989a2d6e9541eb4b6e612428919be7770d
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-12-30 15:12:02 +0100

    Homepages are done
    All that is left is some updating that will be done during the process of User page procedure

--- NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
+++ NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581313601f9f4cae28555058cd2e7a77f4e1a3db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb97d77470fe1135587862246ca3d32706358a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
