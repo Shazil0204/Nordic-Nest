346f9dc1e6b5eaaa067cb47e173d338c5a5420b2
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-01-12 12:08:15 +0100

    update

--- NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
+++ NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e09f9099646eb106a64c8713a4b081b5ad6455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f037cd286ac7aca4fbc3307cc44989bb9dddd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
