ee039dbe7362933ce00dff3caa8b41af8ce53025
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-01-25 21:13:06 +0100

    added message box

---
+++ NordicNest/Pages/ClientCommunication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NordicNest.Pages
+{
+    public class ClientCommunicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
+++ NordicNest/obj/Debug/net8.0/NordicNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3cfe8174b9da7e0a14d5d2c71363493507a441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3f963d17a1f95fa5f54396989ef4ca215d21e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NordicNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
