5c67442f54f1777dbaacd712828da1f3f072ece1
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-02-28 12:25:08 +0100

    update

--- Tailwind/js/filter.js
+++ Tailwind/js/filter.js
@@ -1,3 +1,13 @@
+// Add event listener to button 1
+document.getElementById("filter").addEventListener("click", function () {
+  showOptions();
+});
+
+// Add event listener to button 2
+document.getElementById("currentfilter").addEventListener("click", function () {
+  showOptions();
+});
+
 function showOptions() {
   var filterMenu = document.getElementById("filtermenu");
   var dropdownicon = document.getElementById("dropdownicon");
