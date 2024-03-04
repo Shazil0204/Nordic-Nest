aa8fab6fc0f88b4371e08bb99e49111d15e906f0
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-03-04 15:26:52 +0100

    update

--- Tailwind/js/buttonTextUpdater.js
+++ Tailwind/js/buttonTextUpdater.js
@@ -1,38 +1,41 @@
-// valueChecker.js
-
 // Function to update button text based on user input
 function updateButtonText() {
   // Retrieve user inputs
   const totalAmount = parseFloat(document.getElementById("totalValue").value);
   const startingDate = new Date(document.getElementById("starting-date").value);
-  const deadline = new Date(document.getElementById("deadline").value);
+  const deadlineInput = document.getElementById("deadline").value;
+  const deadline = deadlineInput ? new Date(deadlineInput) : null;
   const amountToInsert = parseFloat(
     document.getElementById("valueToInsert").value
   );
   const frequency = document.getElementById("frequency").value;
 
   // Check if starting date is before the deadline
-  if (startingDate >= deadline) {
+  if (deadline && startingDate >= deadline) {
     // Update button text and disable button
     document.getElementById("calculateBtn").textContent =
       "Dates are causing problems";
     document.getElementById("calculateBtn").style.cursor = "not-allowed";
+    document.getElementById("calculateBtn").disabled = true;
     return; // Exit function
   } else {
-    // Revert button text and enable button
+    // Re-enable button and set default text
     document.getElementById("calculateBtn").textContent = "Submit";
     document.getElementById("calculateBtn").style.cursor = "pointer";
+    document.getElementById("calculateBtn").disabled = false;
   }
 
   // Update button text based on user input
   const calculateBtn = document.getElementById("calculateBtn");
   if (totalAmount && deadline && amountToInsert && frequency) {
-    calculateBtn.textContent = "Calculate";
+    calculateBtn.textContent = "Double check";
   } else if (totalAmount && deadline && frequency) {
     calculateBtn.textContent = `Calculate Value to Insert ${frequency}`;
-  } else if (totalAmount && amountToInsert && frequency) {
+  } else if (totalAmount && amountToInsert && frequency && !deadline) {
+    calculateBtn.textContent = "Calculate deadline";
+  } else if (totalAmount && !deadline && amountToInsert && frequency) {
     calculateBtn.textContent = "Calculate Deadline";
-  } else if (deadline && amountToInsert && frequency) {
+  } else if (deadline && amountToInsert && frequency && !totalAmount) {
     calculateBtn.textContent = "Calculate Total Amount";
   } else {
     calculateBtn.textContent = "Few Data Missing";
